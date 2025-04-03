4d076e39a472d92a2174c70c17327eb459114bab
Xiuwen Zheng <zhengxwen@gmail.com>
2025-04-02 22:32:36 -0500

    update seqListVarData()

--- R/Utilities.R
+++ R/Utilities.R
@@ -10,13 +10,25 @@
 #######################################################################
 
 # R expressions called in C internally
-lang_eval <- list(
+.lang_eval <- list(
     new_rle = quote(new("Rle", values=values, lengths=lengths))
+)
+
+# Compressed Lists in IRanges
+.List_IRanges_value <- list(
+    logical   = LogicalList(),
+    integer   = IntegerList(),
+    numeric   = NumericList(),
+    character = CharacterList(),
+    raw       = RawList(),
+    factor    = FactorList(),
+    "unlistData", "partitioning", "end"
 )
 
 .onLoad <- function(lib, pkg)
 {
-    .Call(SEQ_Pkg_Init, .dim_name, process_count, process_index, lang_eval)
+    .Call(SEQ_Pkg_Init, .dim_name, process_count, process_index,
+        .lang_eval, .List_IRanges_value)
     TRUE
 }
 
